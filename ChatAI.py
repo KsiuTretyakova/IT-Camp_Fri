02ca99e64743283ee31543deb43d11bfc0200e81
KsiuTretyakova <hrytsenkoks@gmail.com>
2023-07-07 19:28:57 +0300

    add .env and finish ChatAI

--- ChatAI.py
+++ ChatAI.py
@@ -1,6 +1,17 @@
 import openai
+import os
 
-openai.api_key = "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"
+from dotenv import load_dotenv as ld
+dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
+if os.path.exists(dotenv_path):
+    ld(dotenv_path)
+
+
+# path=os.path.dirname(__file__).replace('//', '\')
+# dotenv_path=f'{path}\.env'
+
+
+openai.api_key = os.getenv("api_key")
 
 models = openai.Model.list()
 # print(models)
@@ -12,4 +23,11 @@ def handle_input(user_input):
         messages=[{"role": "user", "content": user_input}]
     )
     return copletion
+
+
+# print(handle_input(input()).choices[0].message.content)
+
+while True:
+    print(handle_input(input("You: ")).choices[0].message.content)
+
 
