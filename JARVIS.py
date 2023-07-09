af346c8121d1e2ec806d987c22f4a868d9f42e00
KsiuTretyakova <hrytsenkoks@gmail.com>
2023-07-09 12:57:49 +0300

    finish JARVIS

--- JARVIS.py
+++ JARVIS.py
@@ -4,6 +4,27 @@ import webbrowser
 
 import speech_recognition as sr
 
+# -------------------------------------------------------------------------------
+import openai
+
+from dotenv import load_dotenv as ld
+dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
+if os.path.exists(dotenv_path):
+    ld(dotenv_path)
+
+openai.api_key = os.getenv("api_key")
+
+
+def ai_response(my_task):
+    copletion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": my_task}]
+    )
+    return copletion
+
+#-------------------------------------------------------------------------------
+
+
 # ------------------
 import pyttsx3
 engine = pyttsx3.init()
@@ -22,9 +43,9 @@ def talk(words):
 # talk("Привіт, чим можу допомогти?")
 talk("Hello")
 
-
+r = sr.Recognizer()
 def command():
-    r = sr.Recognizer()
+    global r
 
     with sr.Microphone() as source:
         print("Say")
@@ -33,7 +54,7 @@ def command():
         audio = r.listen(source)
 
     try:
-        # task = r.recognize_google(audio, language="en-US").lower() # ru-RU
+        # task = r.recognize_google(audio, language="en-US").lower()  # ru-RU
         # task = r.recognize_google(audio, language="ru-RU").lower()  # ru-RU
         task = r.recognize_google(audio, language="uk-UA").lower()  # ru-RU
         print("You: " + task)
@@ -45,6 +66,7 @@ def command():
 
 
 def make_something(ar_task):
+    global r
     if ("відкрити" and "сайт") in ar_task:
         talk("ok")
         url = "https://ituniver.com"
@@ -56,6 +78,24 @@ def make_something(ar_task):
 
     elif "ім'я" in ar_task:
         talk("My name is JARVIS")
+
+    else:
+        # print(handle_input(input("You: ")).choices[0].message.content)
+        try:
+            ai_res = ai_response(ar_task).choices[0].message.content
+            talk(ai_res)
+        except openai.error.ServiceUnavailableError:
+            talk("Виникла помилка, спробую ще раз")
+            try:
+                ai_res = ai_response(ar_task).choices[0].message.content
+                talk(ai_res)
+            except openai.error.ServiceUnavailableError:
+                talk("Не можу обробити відповідь, запитай ще раз")
+        except openai.error.RateLimitError:
+            talk("Спробуй через 20 секунд")
+            r.pause_threshold = 20
+        except:
+            talk("Ops, щось пішло не так. Спробуйте ще")
 
 
 while True:
